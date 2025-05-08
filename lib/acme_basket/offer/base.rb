18a79f3351ad1d26f8d0bbce57c73312641299b1
Oare Arene <20444781+oahray@users.noreply.github.com>
2025-05-08 17:32:23 +0100

    Update README

--- lib/acme_basket/offer/base.rb
+++ lib/acme_basket/offer/base.rb
@@ -5,7 +5,7 @@ module AcmeBasket
     # Base class for all offer types.
     class Base
       # This constant should be overridden in subclasses to provide marketing copy.
-      MARKETING_COPY = 'No marketing copy provided'
+      MARKETING_COPY = ''
 
       def apply(items)
         raise NotImplementedError, 'Subclasses must implement `apply`'
