ed51fa0412d59538e7ed1d8c28181fef16d5d0d7
Oare Arene <20444781+oahray@users.noreply.github.com>
2025-05-08 16:53:54 +0100

    Implement delivery charge rules

--- lib/acme_basket.rb
+++ lib/acme_basket.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require 'acme_basket/product'
+require 'acme_basket/catalogue/base'
+require 'acme_basket/catalogue/widget_catalogue'
+require 'acme_basket/delivery_rule/widget_charge'
+require 'acme_basket/offer/base'
+require 'acme_basket/offer/buy_red_widget_get_second_half'
+require 'acme_basket/basket'
+
 # Main module for the AcmeBasket gem.
 module AcmeBasket
 end
